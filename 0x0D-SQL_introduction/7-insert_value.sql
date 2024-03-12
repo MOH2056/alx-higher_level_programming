77956da1bcd8e8d0465aeb1d2b04385c9c6f86fa
MOH2056 <tapereyetunde@gmail.com>
2024-03-12 14:37:39 -0700

    A script that insert a new row in a table

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,3 @@
 -- a script that inserts a new row in the table first_table (database hbtn_0c_0) in your MySQL server.
 
-INSERT INTO 'fisrt_table' ('id', 'name') VALUES (89, "Best School");
+f'INSERT INTO fisrt_table (id, name) VALUES (%s, %s)'
